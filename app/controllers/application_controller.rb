ce7cbe5a1cce70b7c62591fc7750ca216a416560
Oleg-rb <oleg1107@rambler.ru>
2020-05-18 15:47:11 +0300

    Permit username for sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?    # применяем метод фильтрации только в контроллере devise
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up)  << :username    # добавляем значение username в хеш
+  end
 end
