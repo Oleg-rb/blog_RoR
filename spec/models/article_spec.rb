666a0d0ea4f0e9fe3e065c352fe26fadf031071d
Oleg-rb <oleg1107@rambler.ru>
2020-05-22 14:49:14 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -12,9 +12,17 @@ describe Article do
 
   describe "#subject" do
     it "returns the article title" do
-      article = create(:article, title: 'Foo')        #создаём объект не с помощью ActiveRecord, а хитрым способом
+      article = create(:article, title: 'Foo')         #создаём объект не с помощью ActiveRecord, а хитрым способом
      
-      expect(article.subject).to eq 'Foo'             #assert, проверка
+      expect(article.subject).to eq 'Foo'              #assert, проверка
+    end
+  end
+
+  describe "#last_comment" do
+    it "returns the last comment" do
+      article = create(:article_with_comments)                  # создаём статью с 3 комментариями
+      
+      expect(article.last_comment.body).to eq "Comment body 3"  # проверка
     end
   end
 end
