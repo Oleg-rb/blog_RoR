ff9d6415de32ddfbf14ba955ce504b14163dda2a
Oleg-rb <oleg1107@rambler.ru>
2020-05-23 18:17:26 +0300

    Update create contact test

--- spec/features/visitor_creates_contact_spec.rb
+++ spec/features/visitor_creates_contact_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 feature "Contact creation" do                       # создание контакта
   scenario "allows acess to contacts page" do       # проверка доступа к странице (что эта страница открывается)
-    visit '/contact'                                # или можно visit new_contacts_path
+    visit new_contacts_path                         # или можно visit '/contact'
 
-    expect(page).to have_content 'Contact us'
+    expect(page).to have_content I18n.t 'contacts.contact_us'
   end
 end
