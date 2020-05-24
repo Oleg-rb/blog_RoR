c3fb15decd6007dc684a13570796bb6d86a5a3dd
Oleg-rb <oleg1107@rambler.ru>
2020-05-24 19:04:17 +0300

    Refactoring: move sign_up to spec/rails_helper.rb

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -2,15 +2,8 @@ require "rails_helper"
 
 feature "Account Creation" do
   scenario "allows guest to create account" do
-    visit new_user_registration_path
+    sign_up
 
-    fill_in :user_username, with: 'FooBar'
-    fill_in :user_email, with: 'foo@bar.com'
-    fill_in :user_password, with: '1234567'
-    fill_in :user_password_confirmation, with: '1234567'
-
-    click_button 'Sign up'
-    
     expect(page).to have_content I18n.t('devise.registrations.signed_up')   # взято из i18n - config/locales/devise.en.yml
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,3 +73,14 @@ end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
+
+def sign_up
+  visit new_user_registration_path
+
+  fill_in :user_username, with: 'FooBar'
+  fill_in :user_email, with: 'foo@bar.com'
+  fill_in :user_password, with: '1234567'
+  fill_in :user_password_confirmation, with: '1234567'
+
+  click_button 'Sign up'
+end
