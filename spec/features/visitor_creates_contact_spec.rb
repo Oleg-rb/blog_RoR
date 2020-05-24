34ef942bc6c6671caff8156b0d189290b5be8d91
Oleg-rb <oleg1107@rambler.ru>
2020-05-24 16:20:09 +0300

    Update test (allows a guest to create contact)

--- spec/features/visitor_creates_contact_spec.rb
+++ spec/features/visitor_creates_contact_spec.rb
@@ -1,9 +1,20 @@
 require "rails_helper"
 
 feature "Contact creation" do                       # создание контакта
-  scenario "allows acess to contacts page" do       # проверка доступа к странице (что эта страница открывается)
-    visit new_contacts_path                         # или можно visit '/contact'
+  scenario "allows access to contacts page" do      # проверка доступа к странице (что эта страница открывается)
+    visit new_contacts_path                         # используем путь из хелпера в Routes
 
     expect(page).to have_content I18n.t 'contacts.contact_us'
+  end
+
+  scenario "allows a guest to create contact" do
+    visit new_contacts_path
+
+    fill_in :contact_email, :with => 'user@example.com'
+    fill_in :contact_message, :with => 'something'
+    
+    click_button 'Send message'
+
+    expect(page).to have_content 'Thanks!'          # то, что ожидаем после совершения действия на странице
   end
 end
